69d09f6fc22f615531d07502dac68dfc009b6326
Mrydv <126314615+Mrydv@users.noreply.github.com>
2024-12-31 09:04:43 +0530

    Update UsrController.java
    Serialized into the response body.

--- src/main/java/com/Org3/Organization3/UsrController/UsrController.java
+++ src/main/java/com/Org3/Organization3/UsrController/UsrController.java
@@ -1,10 +1,13 @@
 package com.Org3.Organization3.UsrController;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Org3.Organization3.Models.Usr;
@@ -17,15 +20,43 @@ public class UsrController {
 	@Autowired
 	private usrService usrsrvc;
 	
+	//Sim Get endpoint
+//	@GetMapping("/Dtls")
+//	public Iterable<Usr> getAllUse(){
+//		return usrsrvc.getAllUsrs();
+//	}
+	
 	
 	@GetMapping("/Dtls")
-	public Iterable<Usr> getAllUsr(){
-		return usrsrvc.getAllUsrs();
+	public @ResponseBody ResponseEntity<?> getAllUsr(){
+		Iterable<Usr> usrlstObj= (Iterable<Usr>) usrsrvc.getAllUsrs();
+		if(usrlstObj!=null) {
+			return new ResponseEntity<>(usrlstObj, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
 	}
 	
+	
+	//Sim Post endpoint
+//	@PostMapping("/post")
+//	public void createUsr(@RequestBody Usr usr) {
+//		usrsrvc.createUsr(usr);
+//	}
+	
+	
+	
 	@PostMapping("/post")
-	public void createUsr(@RequestBody Usr usr) {
-		usrsrvc.createUsr(usr);
+	public @ResponseBody ResponseEntity<?> createUsr(@RequestBody Usr usr) {
+		if(usr!=null) {
+			 usrsrvc.createUsr(usr);
+			return new ResponseEntity<>(usr, HttpStatus.CREATED);  
+		}else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
 	}
+	
+	
 
 }
